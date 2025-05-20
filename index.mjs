b10dd6df506b9cd97202ca950fef31d72bb4b914
Vince <vincentmichaelpalma@gmail.com>
2025-05-20 04:50:24 -0700

    Initial commit

---
+++ index.mjs
@@ -0,0 +1,38 @@
+import express from 'express';
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+//for Express to get values using POST method
+app.use(express.urlencoded({extended:true}));
+
+//setting up database connection pool
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  connectionLimit: 10,
+  waitForConnections: true
+});
+const conn = await pool.getConnection();
+
+//routes
+app.get('/', (req, res) => {
+   res.send('Hello Express app!')
+});
+
+app.get("/dbTest", async(req, res) => {
+    let sql = "SELECT CURDATE()";
+    const [rows] = await conn.query(sql);
+    res.send(rows);
+});//dbTest
+
+app.listen(3000, ()=>{
+    console.log("Express server running")
+})
